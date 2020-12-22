2f99c841f3100a7bdacc266115346378f9787457
kloutw <kloutw@gmail.com>
2020-12-22 16:41:16 +0800

    querycast function use

--- R-db_types/reshape_dcast2.R
+++ R-db_types/reshape_dcast2.R
@@ -9,17 +9,18 @@ dbListTables(mysqlconnection)
 ################################################
 # define a function to cast the df to mat
 ################################################
-query_cast = function(query_result){
-    query_df=fetch(query_result, n = -1) 
-    mycast=dcast(query_df,
-                 firstname~Month,
-                 fun.aggregate = sum,
-                 value.var = "num",
-                 fill = NA_real_)
-    mymat=as.matrix(mycast[,-1])
-    rownames(mymat) = mycast$firstname
-    return(mymat)
-}
+#query_cast = function(query_result){
+#    query_df=fetch(query_result, n = -1) 
+#    mycast=dcast(query_df,
+#                 firstname~Month,
+#                 fun.aggregate = sum,
+#                 value.var = "num",
+#                 fill = NA_real_)
+#    mymat=as.matrix(mycast[,-1])
+#    rownames(mymat) = mycast$firstname
+#    return(mymat)
+#}
+source("querycast.R")
 ################################################
 # query db
 ################################################
