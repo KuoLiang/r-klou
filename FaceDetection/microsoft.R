b2c694b0c3491f4a398ae754e64789bc41edbfe8
Kuo-Liang Ou <kloutw@gmail.com>
2021-01-13 00:21:36 +0800

    Update microsoft.R

--- FaceDetection/microsoft.R
+++ FaceDetection/microsoft.R
@@ -2,6 +2,13 @@
 library(tidyverse)
 library(httr)
 library(stringr)
+img.url = 'http://ilst.site.nthu.edu.tw/var/file/276/1276/pictures/930/m/mczh-tw250x250_small111732_366078584785.jpg'
+
+# Define Microsoft API URL to request data
+URL.emoface = 'https://southcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=emotion&recognitionModel=recognition_03&returnRecognitionModel=false&detectionModel=detection_01&faceIdTimeToLive=86400'
+
+# Define access key (access key is available via: https://www.microsoft.com/cognitive-services/en-us/emotion-api)
+emotionKEY = '543a13cee1024b1483a9c688942d4172'
 
 api_url <- "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?visualFeatures=Faces"
 
@@ -29,3 +36,4 @@ get_emotions_microsoft <- function(image_url, api_key){
         mutate(value = value %>% round(2)) # Round values for 2 decimals
 }
 
+get_emotions_microsoft(img.url,emotionKEY)
