b2b5cd2ea328775c4828d175e17c2011ee9b4ea0
Kuo-Liang Ou <kloutw@gmail.com>
2021-01-12 23:54:41 +0800

    Create opencv.R

---
+++ FaceDetection/opencv.R
@@ -0,0 +1,12 @@
+install.packages("opencv")
+library(opencv)
+unconf <- ocv_read('http://ilst.site.nthu.edu.tw/var/file/276/1276/pictures/930/m/mczh-tw250x250_small111732_366078584785.jpg')
+faces <- ocv_face(unconf)
+ocv_write(faces, 'faces.jpg') #write a circle to working directory
+facemask <- ocv_facemask(unconf)
+attr(facemask, 'faces')
+### live camera
+ocv_video(ocv_face)
+ocv_video(ocv_edges)
+###
+
