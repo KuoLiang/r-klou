e903c22f3ad6434633d94720c5808962973d97f9
Kuo-Liang Ou <kloutw@gmail.com>
2021-10-26 22:17:04 +0800

    Update dataframe.R

--- FirstStep/dataframe.R
+++ FirstStep/dataframe.R
@@ -2,21 +2,21 @@ a = c(1,2,3)
 b = c(4,5,6)
 c = c("a","b","c")
 
-df = data.frame(a,b,c)
-df
-df$a            #numeric
-df$c            #character
-df[1,]          #numeric
-df[,1]          #numeric
-df[1,1]         #numeric
-df[1]           #data.frame
-df[1,]          #data.frame
-df[,1:3]        #data.frame
+mydf = data.frame(a,b,c)
+mydf
 
-class(df$a)     #numeric
-class(df$c)     #character
-class(df[,1])   #numeric
-class(df[1,1])  #numeric
-class(df[1])    #data.frame
-class(df[1,])   #data.frame
-class(df[,1:3]) #data.frame
+mydf$a            #numeric
+mydf$c            #character
+
+mydf[2,]          ##data.frame  某一列,跨不同行
+mydf[,2]          #numeric      某一行,vector 需相同 type
+mydf[,3]          #character    某一行,vector 需相同 type
+
+mydf[1,1]         #numeric      某一個值
+mydf[1,3]         #character    某一個值
+
+mydf[2]           #data.frame   請比較mydf[,2]
+mydf[,1:2]        #data.frame
+
+class(mydf[2])     #reveal the data type
+
