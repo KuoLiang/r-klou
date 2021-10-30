17f7bb5b687f6ea779cea0c69d2fa6b1dd1a1653
Kuo-Liang Ou <kloutw@gmail.com>
2021-10-30 23:51:19 +0800

    a

--- TextMining/wordcloud.R
+++ TextMining/wordcloud.R
@@ -1,19 +1,35 @@
-install.packages("wordcloud2")
+if (!require(devtools))  install.packages("devtools")
+devtools::install_github('lchiffon/wordcloud2')
+#install.packages("wordcloud2")
 library(wordcloud2)
+
 wordcloud2(data = demoFreq)
 
-mycolor = c(seq(0:800))
+mydemoFreq = demoFreq
 mydemoFreqC = demoFreqC
 
-mydemoFreqC = cbind(demoFreqC, colorlist = mycolor)
+wordcloud2(mydemoFreq, size = 2, 
+           color = "random-light", 
+           backgroundColor = "grey")
+wordcloud2(mydemoFreqC, size = 2, fontFamily = "微軟雅黑",
+           color = "random-light", backgroundColor = "black")
 
-wordcloud2(demoFreq, size = 2, fontFamily = "微軟雅黑", color = "random-light", backgroundColor = "grey")
+mycolor = c(rgb(0,0,(884:0)/885))
+#mycolor = c(gray(seq(0,1,length.out = 885)))
 
-wordcloud2(demoFreq, size = 2, color = "random-light", backgroundColor = "grey")
-wordcloud2(demoFreqC, size = 2, 
+colorlist = mycolor 
+mydemoFreqC = cbind(demoFreqC, colorlist)
+wordcloud2(mydemoFreqC, size = 2, 
            color = mydemoFreqC$colorlist, 
-           backgroundColor = "black",)
+           backgroundColor = "white",)
 
 demoFreq
 demoFreqC
 mydemoFreqC
+
+
+csphd <- function(colors)
+  barplot(csd, col = colors, ylim = c(0,30),
+          names.arg = 72:85, xlab = "Year", ylab = "Students",
+          legend.text = c("Winter", "Spring", "Summer", "Fall"),
+          main = "Computer Science PhD Graduates", las = 1)
