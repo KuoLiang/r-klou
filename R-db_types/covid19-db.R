cc72a7266a192e9910107ded55dd1da916926916
Kuo-Liang Ou <kloutw@gmail.com>
2021-10-31 16:29:35 +0800

    Update covid19-db.R

--- R-db_types/covid19-db.R
+++ R-db_types/covid19-db.R
@@ -6,11 +6,17 @@ connect = dbConnect(RMySQL::MySQL(), dbname = "northwind",username = "student",
 dbListTables(connect)
 NW = dbGetQuery(connect ,"select * from orders")
 NW
-
+class(NW)
+############
 connect2 = dbConnect(RMySQL::MySQL(), dbname = "covid19",username = "student", 
-                     password = "0000",host = "120.127.169.83")
+                     password = "35162",host = "120.127.169.83")
 dbListTables(connect2)
+
+CT = dbGetQuery(connect2, "SET NAMES 'utf8'") 
 CT = dbGetQuery(connect2 ,"select * from cases_tw")
 CT
+CT = dbGetQuery(connect2 ,"select id,國籍別,性別 from cases_tw")
+CT
 class(CT)
-class(NW)
+CT$國籍別=as.factor(CT$國籍別)
+CT$性別=as.factor(CT$性別)
