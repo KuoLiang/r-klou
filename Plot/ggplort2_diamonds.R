3e4b3451d3c1002cd15fbc69b0419faf1cf6db4a
Kuo-Liang Ou <kloutw@gmail.com>
2021-11-12 19:53:20 +0800

    Update ggplort2_diamonds.R

--- Plot/ggplort2_diamonds.R
+++ Plot/ggplort2_diamonds.R
@@ -8,18 +8,18 @@ z1=diamonds[diamonds$color=="D",]
 z2=subset(diamonds, color=="D" | color=="E")
 z2=subset(diamonds, color=="D" | color=="E" & depth > 60)
 
-plot(x)   #x 為重量，num
-plot(y)   #y 為價錢，int
-plot(z)   #z 為色澤，factor
+plot(x)   #x ?????um
+plot(y)   #y ???嚗nt
+plot(z)   #z ??瞉歹?actor
 plot(x,y) #xy vector
 plot(x,y,col=z) #xy vector
 plot(y,x) #yx vector
 plot(z,y) #z factor;  y vetor boxplot
 plot(y,z) #y vector;  z factor plot  
 plot(z1,y)
-#心得：X軸為類別型，Y軸為數值，會有 box統計
-#心得：X軸為數值，就是各點分配圖
-plot(diamonds) #請小心使用 same as the "pairs(d)"
+#敹??頠貊憿???頠貊??潘???? box蝯梯??
+#敹??頠貊??潘?停????????
+plot(diamonds) #隢??蝙? same as the "pairs(d)"
 plot(~x)
 plot(~z)  
 plot(y~z) #same as plot(z,y)
