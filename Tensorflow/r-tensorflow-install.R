1d653e0924dbdf5bdf9ef443d8077d017f38b462
Kuo-Liang Ou <kloutw@gmail.com>
2021-11-28 14:04:41 +0800

    Create r-tensorflow-install.R

---
+++ Tensorflow/r-tensorflow-install.R
@@ -0,0 +1,9 @@
+install.packages("tensorflow")
+install.packages("keras")
+library(tensorflow)
+library(keras)
+install_tensorflow()
+
+#install_keras() 
+#install_tensorflow(version = "gpu")
+tf$constant("Hellow Tensorflow")
