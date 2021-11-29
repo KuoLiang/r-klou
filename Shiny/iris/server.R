4e9dac2b272d47c7537ad2378081951ece8b181f
Kuo-Liang Ou <kloutw@gmail.com>
2021-11-29 23:31:06 +0800

    Update server.R

--- Shiny/iris/server.R
+++ Shiny/iris/server.R
@@ -28,10 +28,10 @@ shinyServer(function(input, output) {
   #scatterplot3d(iris)
   output$plot2 <- renderPlot({
     colors <- c("red", "green", "blue")
-    colors <- colors[as.numeric(iris$Species)]  #依品種編號123
-    p3 <- scatterplot3d(iris[,1:3], pch = 16, color=colors)
+    colors_num <- colors[as.numeric(iris$Species)]  #依品種編號123
+    p3 <- scatterplot3d(iris[,1:3], pch = 16, color=colors_num)
     legend("right", legend = levels(iris$Species),
-           col =  c("#999999", "#E69F00", "#56B4E9"), pch = 16)
+           col =  colors, pch = 16)
     print(p3)
   })
   
