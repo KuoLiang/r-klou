7dbbffa2e7cf3944538aaf177c2a34e159226200
Kuo-Liang Ou <kloutw@gmail.com>
2021-11-29 19:02:18 +0800

    a

--- Shiny/iris-cluster/server.R
+++ Shiny/iris-cluster/server.R
@@ -1,16 +1,23 @@
 library(datasets)
+i = 0
 function(input, output, session) {
   
   # Combine the selected variables into a new data frame
   selectedData <- reactive(
     {
-      iris[, c(input$xcol, input$ycol)]
-    })
+      iris[, c(input$xcol, input$ycol)] 
+      #使用者在 ui.R 選擇的XY軸,作為 iris 欄位篩選用
+    }) #set the reactive function as a function return to selectData()
   
   clusters <- reactive(
     {
       kmeans(selectedData(), input$clusters)
-    })
+      #使用者在 ui.R 選擇的 cluster number 作為 kmeans 的第二個參數
+    }) #set the reactive function as a function return to clusters()
+  
+  output$text = renderText({
+    print(input$clusters)
+  })
   
   output$plot1 <- renderPlot({
     palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
@@ -19,8 +26,8 @@ function(input, output, session) {
     par(mar = c(5.1, 4.1, 0, 1)) #set the margin
     
     plot(selectedData(),
-         col = clusters()$cluster,
-         pch = 20, cex = 3)
+         col = clusters()$cluster,  #color
+         pch = 20, cex = 3)         #set the point character and cex expand
     #col : color
     #pch : plot character 4 = X ; 3 = + ; etc.
     #cex : expand the mark
--- Shiny/iris-cluster/ui.R
+++ Shiny/iris-cluster/ui.R
@@ -3,22 +3,33 @@
   #在版型(layout)中再使用版面(panel)函數進階設定
   #sidebarLayout()函數中常用sidebarPanel()與mainPanel()
 
-
-# k-means only works with numerical variables,
-# so don't give the user the option to select
-# a categorical variable
 library(datasets)
 
 vars <- setdiff(names(iris), "Species")
 
 pageWithSidebar(
-  headerPanel('Iris k-means clustering'),
+  headerPanel('Iris k-means clustering'),  #the title
+  
+  #read users' input
   sidebarPanel(
-    selectInput('xcol', 'X Variable', vars),
+    selectInput('xcol', 'X Variable', vars), #input id , labels, choices vector
     selectInput('ycol', 'Y Variable', vars, selected = vars[[2]]),
-    numericInput('clusters', 'Cluster count', 3, min = 1, max = 9)
+    #numericInput('clusters', 'Cluster count', 3, min = 1, max = 9)
+    sliderInput('clusters','Cluster count', min=1, max=9, value=3),
+    
+    textInput("txt", "Enter the text to display below:"),
+    
+    #文字區給 server.R 使用
+    textOutput("text"),
+    verbatimTextOutput("verb")
   ),
+  #textOutput() is usually paired with renderText() in server.R
+  #and puts regular text in <div> or <span>; 
+  #verbatimTextOutput() is usually paired with renderPrint() in server.R
+  #and provides fixed-width text in a <pre>.
+
+  #圖形區；plot1 給 server.R 使用
   mainPanel(
-    plotOutput('plot1')
+    plotOutput('plot1')     
   )
 )
