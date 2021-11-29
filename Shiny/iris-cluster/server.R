25fc9a96e391de050fde7649dc0f003b979a7d40
Kuo-Liang Ou <kloutw@gmail.com>
2021-11-29 20:55:53 +0800

    a

--- Shiny/iris-cluster/server.R
+++ Shiny/iris-cluster/server.R
@@ -1,26 +1,30 @@
 library(datasets)
-i = 0
+
 function(input, output, session) {
   
   # Combine the selected variables into a new data frame
   selectedData <- reactive(
     {
-      iris[, c(input$xcol, input$ycol)] 
+      iris[, c(input$x, input$y)] 
       #使用者在 ui.R 選擇的XY軸,作為 iris 欄位篩選用
     }) #set the reactive function as a function return to selectData()
   
   clusters <- reactive(
     {
-      kmeans(selectedData(), input$clusters)
+      kmeans(selectedData(), input$slide_num)
       #使用者在 ui.R 選擇的 cluster number 作為 kmeans 的第二個參數
     }) #set the reactive function as a function return to clusters()
   
-  output$text = renderText({
-    print(input$clusters)
-  })
+  output$out_text = renderText({
+    print(input$slide_num)
+  }) #使用者輸入 slider 顯示
+  
+  output$out_text2 = renderText({
+    print(input$text)
+  }) #使用者輸入的text顯示
   
   output$plot1 <- renderPlot({
-    palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+    palette(c("red", "blue", "green", "purple",
               "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
     
     par(mar = c(5.1, 4.1, 0, 1)) #set the margin
--- Shiny/iris-cluster/ui.R
+++ Shiny/iris-cluster/ui.R
@@ -5,23 +5,23 @@
 
 library(datasets)
 
-vars <- setdiff(names(iris), "Species")
+vars <- setdiff(names(iris), "Species") #直接使用資料集的欄位名作為下拉選單
+                                        #除了 Species外
 
 pageWithSidebar(
   headerPanel('Iris k-means clustering'),  #the title
   
   #read users' input
   sidebarPanel(
-    selectInput('xcol', 'X Variable', vars), #input id , labels, choices vector
-    selectInput('ycol', 'Y Variable', vars, selected = vars[[2]]),
+    selectInput('x', 'X Variable', vars), #input id , labels, choices vector
+    selectInput('y', 'Y Variable', vars, selected = vars[[2]]),
+    hr(),  #水平分隔線
     #numericInput('clusters', 'Cluster count', 3, min = 1, max = 9)
-    sliderInput('clusters','Cluster count', min=1, max=9, value=3),
-    
-    textInput("txt", "Enter the text to display below:"),
-    
-    #文字區給 server.R 使用
-    textOutput("text"),
-    verbatimTextOutput("verb")
+    sliderInput('slide_num','Cluster count', min=1, max=9, value=3),
+    textOutput("out_text"),       #文字區給 server.R 使用
+    hr(),  #水平分隔線
+    textInput("text", "輸入文字測試"),
+    textOutput("out_text2")    #文字區給 server.R 使用
   ),
   #textOutput() is usually paired with renderText() in server.R
   #and puts regular text in <div> or <span>; 
