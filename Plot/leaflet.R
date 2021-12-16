bba9c49158f77e5cf2cc35ae536191b43f775459
Kuo-Liang Ou <kloutw@gmail.com>
2021-12-16 22:10:19 +0800

    Create leaflet.R

---
+++ Plot/leaflet.R
@@ -0,0 +1,16 @@
+install.packages("leaflet")
+library(leaflet)
+
+m <- leaflet() %>%
+  addTiles() %>%  # Add default OpenStreetMap map tiles
+  addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
+m
+#########
+nthu <- leaflet()
+nthu <- addTiles(nthu)
+nthu <- addMarkers(nthu, lng=120.9967, lat=24.7961, popup="The NTHU Campus")
+nthu
+#########
+
+
+
