54b1d6ebc2a08c902bd2d8e7caae9560ad28abf4
Kuo-Liang Ou <kloutw@gmail.com>
2022-09-27 22:15:40 +0800

    Update ggplot2-nw.R

--- Plot/ggplot2-nw.R
+++ Plot/ggplot2-nw.R
@@ -1,11 +1,20 @@
-'''
-SELECT e.EmployeeID, Year(o.OrderDate),o.ShipCountry, od.Quantity*od.UnitPrice as , od.Discount, o.Freight
-FROM employees as e, orders as o, `order details` as od
-WHERE e.EmployeeID=o.EmployeeID
-AND o.OrderID = od.OrderID
-'''
+myquery =
+"SELECT e.EmployeeID, Year(o.OrderDate) as Year,o.ShipCountry, od.Quantity*od.UnitPrice as Sales,od.Discount, o.Freight
+FROM employees as e, orders as o, orderdetails as od
+WHERE e.EmployeeID=o.EmployeeID AND o.OrderID = od.OrderID"
 
+#myquery = "select EmployeeID from employees"
 nw = read.csv("/Users/klou/Downloads/e.csv")
+
+install.packages("RMySQL")
+library("RMySQL")
+mysqlconnection = dbConnect(MySQL(), user = 'student', password = 's?P%3p7DeGw5H#HM', dbname = 'northwind' ,host = '10.8.0.1')
+dbListTables(mysqlconnection)
+
+result = dbSendQuery(mysqlconnection, myquery)
+nw = fetch(result, n=-1)
+nw
+
 nw$Year = factor(nw$Year)
 nw$EmployeeID = factor(nw$EmployeeID)
 library(ggplot2)
