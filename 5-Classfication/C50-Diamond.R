3b9884817690baaaaa70673eb5699f9ce2b3c39d
Kuo-Liang Ou <kloutw@gmail.com>
2022-11-29 13:44:18 +0800

    a

--- 5-Classfication/C50-Diamond.R
+++ 5-Classfication/C50-Diamond.R
@@ -1,24 +1,24 @@
 install.packages("ggplot2")
-install.packages("C50")
-library(C50)
+
 library(ggplot2)
+library(rpart)
+library(rpart.plot)
+
 data(diamonds)
 sample_index <- sample(1:nrow(diamonds), size = 100)
 sample_diamonds <- diamonds[sample_index,]
 input <- sample_diamonds[,c(1,2,4,5,6,7,8,9,10)]
-model1 <- C5.0(input, sample_diamonds$color, control = C5.0Control(noGlobalPruning = FALSE))
-model2 <- C5.0(color ~. , sample_diamonds, control = C5.0Control(noGlobalPruning = FALSE))
 
-summary(model1)
-summary(model2)
+model <- rpart(color ~., data = sample_diamonds, method = 'class')
+rpart.plot(model, extra = 100)
 
-#plot(model1, main="C5.0 Decision Tree, min=1")
+summary(model)
 
 sample_index2 <- sample(1:nrow(diamonds), size = 10)
 sample_index2
 newcases <- diamonds[sample_index2,-3]
 newcases
-predicted <- predict(model2, newcases, type="prob")
-predicted <- predict(model2, newcases, type="class")
+predicted <- predict(model, newcases, type="prob")
+predicted <- predict(model, newcases, type="class")
 
 predicted
