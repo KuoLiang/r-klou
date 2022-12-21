d38a5d565adc5529c6cb31f7302925657ee1d613
Kuo-Liang Ou <kloutw@gmail.com>
2022-12-21 22:07:07 +0800

    Update 1-KMeans-HClust-Iris.R

--- 6-Clustering/1-KMeans-HClust-Iris.R
+++ 6-Clustering/1-KMeans-HClust-Iris.R
@@ -3,14 +3,14 @@
 #National Tsing Hua University ILST
 ##########################################
 # k-means
-k_result <- kmeans(iris[,-5], 3)
-k_result$cluster
-k_result$centers
-k_result$size
-table(iris$Species,k_result$cluster)
+k_result <- kmeans(iris[,-5], 3) #去除非數值欄位後分三群
+k_result$cluster # 回傳 vector 1 to k
+k_result$centers # 中心點
+k_result$size    # 各群數量
+table(iris$Species,k_result$cluster) #confusion matrix
 
 plot(iris[c("Sepal.Length","Sepal.Width")],
-     col=kmeans.result$cluster)
+     col=k_result$cluster) # color 設定為群代號
 points(k_result$centers[,c("Sepal.Length","Sepal.Width")],
        col=1:3, pch=2)
 
