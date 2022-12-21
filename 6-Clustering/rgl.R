0b432c6d4c9174e21165a955f11bf1ed8f78aa3a
Kuo-Liang Ou <kloutw@gmail.com>
2022-12-21 22:55:19 +0800

    a

--- 6-Clustering/rgl.R
+++ 6-Clustering/rgl.R
@@ -1,7 +1,7 @@
-# library #install.packages("rgl")
+
+#install.packages("rgl")
 options(rgl.useNULL = TRUE)
 library(rgl)
-rgl.open()
 # This is to output a rgl plot in a rmarkdown document.
 # setupKnitr()
 
@@ -25,7 +25,7 @@ plot3d(
 
 # To save to a file:
 htmlwidgets::saveWidget(rglwidget(width = 520, height = 520), 
-                        file = "HtmlWidget/3dscatter.html",
+                        file = "3dscatter.html",
                         libdir = "libs",
                         selfcontained = FALSE
 )
