12c9af128684bcd60cf88582a398f9df7ee7f87f
Kuo-Liang Ou <kloutw@gmail.com>
2022-12-28 22:57:30 +0800

    a

--- 8-Evaluation/1-PR_ROC.R
+++ 8-Evaluation/1-PR_ROC.R
@@ -1,5 +1,13 @@
+##########################################
+#Editor: Kuo-Liang Ou
+#National Tsing Hua University ILST
+##########################################
+#ROC = the probability curve of the values 
+#AUC = separability of the different groups of values/labels.
 install.packages("PRROC")
 library(PRROC)
+install.packages("verification")
+library(verification)
 
 # random generate C1
 C1 = rnorm(300); #mean = 0 ; sd =1
@@ -9,31 +17,31 @@ summary(C1)
 C2 = rnorm(300,-2); #mean = -2 ; sd =1
 C2
 summary(C2)
-
 #random generate C3
 C3 = rnorm(300,2,2); #mean = 2 ; sd =2
 C3
 summary(C3)
-
 ### 觀察C1C2C3的分布
 boxplot(C1,C2,C3)
 
 ### C1 C2 ROC and threshold
-roc = roc.curve(C1, C2, curve = TRUE) #curve must be T
-roc
-plot(roc)
+#Sensitividy = TPR
+roc_12 = roc.curve(C1, C2, curve = TRUE) #curve must be T
+roc_12
+plot(roc_12)
 
 ### C1 C3 ROC and threshold
-roc = roc.curve(C1, C3, curve = TRUE) #curve must be T
-roc
-plot(roc)
+roc_13 = roc.curve(C1, C3, curve = TRUE) #curve must be T
+roc_13
+plot(roc_13)
 
 ### C2 C3 ROC and threshold
-roc = roc.curve(C2, C3, curve = TRUE) #curve must be T
-roc
-plot(roc)
-
+roc_23 = roc.curve(C2, C3, curve = TRUE) #curve must be T
+roc_23
+plot(roc_23)
+#################################
 ### PR Curve - Precision-Recall
+#################################
 pr = pr.curve(C1, C2,curve = TRUE) #C1 positive C2 negative
 pr
 plot(pr)
