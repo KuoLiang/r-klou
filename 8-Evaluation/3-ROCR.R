11256f8f2d80b0d581642ac7acc8db79068c6cf2
Kuo-Liang Ou <kloutw@gmail.com>
2022-12-29 00:16:00 +0800

    a

--- 8-Evaluation/3-ROCR.R
+++ 8-Evaluation/3-ROCR.R
@@ -5,13 +5,23 @@ ROCR.simple      #200 samples of 1 and 0
 ROCR.simple$predictions
 ROCR.simple$labels
 pred <- prediction( ROCR.simple$predictions, ROCR.simple$labels) 
-perf <- performance(pred,"tpr","fpr")
+perf <- performance(pred,"tpr","fpr") 
 perf
 plot(perf)
+#ROC curves: measure="tpr", x.measure="fpr".
+#Precision/recall graphs: measure="prec", x.measure="rec". 
+#Sensitivity/specificity plots: measure="sens", x.measure="spec".
+
 perf2 <- performance(pred, measure = "auc")   #area under curve
 perf2@y.values
-###
+###                                      
+# acc: accuracy
+# rec: recall
+# prec: precision
+perf3 <- performance(pred, measure = "rec")   #area under curve
+perf3@y.values
 
+###################################
 data(ROCR.hiv)
 attach(ROCR.hiv)
 pred.svm <- prediction(hiv.svm$predictions, hiv.svm$labels)
---
+++ 8-Evaluation/4-ROCR_detail.R
@@ -0,0 +1,51 @@
+install.packages("ROCR")
+library(ROCR)
+data(ROCR.simple)
+library(e1071)
+
+group=c(rep(0,times=100),rep(1,times=100))
+#pred <- prediction( ROCR.simple$labels, ROCR.simple$labels) 
+pred <- prediction( rnorm(200,mean=1,sd=1), ROCR.simple$labels) 
+pred <- prediction( rnorm(200,mean=2,sd=2), ROCR.simple$labels) 
+
+pred <- prediction( rnorm(200,mean=1,sd=1), group) 
+pred <- prediction( rnorm(200,mean=2,sd=2), group) 
+
+# prediction 第一個參數位資料集，第二個參數為組別，目前支援二分法
+# 多組以上需兩兩比較
+
+perf <- performance(pred,"tpr","fpr") 
+perf
+plot(perf)
+perf <- performance(pred, measure = "auc")   #area under curve
+perf@y.values
+
+#########
+#for IRIS
+#########
+iris$setosa <- as.numeric(iris$Species == "setosa")
+mod1 <- svm(iris$setosa~Sepal.Length+Sepal.Width, data=iris, kernel="radial")
+
+pred1 <- prediction(predict(mod1), iris$setosa)
+perf1 <- performance(pred1,"tpr","fpr")
+plot(perf1)
+perf1 <- performance(pred1, measure = "auc")   #area under curve
+perf1@y.values
+#############
+iris$vcolor <- as.numeric(iris$Species == "versicolor")
+mod2 <- svm(iris$vcolor~Sepal.Length+Sepal.Width, data=iris, kernel="radial")
+
+pred2 <- prediction(predict(mod2), iris$vcolor)
+perf2 <- performance(pred2,"tpr","fpr")
+plot(perf2)
+perf2 <- performance(pred2, measure = "auc")   #area under curve
+perf2@y.values
+#############
+iris$vnica <- as.numeric(iris$Species == "virginica")
+mod3 <- svm(iris$vnica~Sepal.Length+Sepal.Width, data=iris, kernel="radial")
+
+pred3 <- prediction(predict(mod3), iris$vnica)
+perf3 <- performance(pred3,"tpr","fpr")
+plot(perf3)
+perf3 <- performance(pred3, measure = "auc")   #area under curve
+perf3@y.values
