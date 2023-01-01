71ec19ee024d42da0bf70685bf4d4eb730551c40
Kuo-Liang Ou <kloutw@gmail.com>
2023-01-01 20:27:29 +0800

    a

--- 14-deployment/deployment.R
+++ 14-deployment/deployment.R
@@ -1,10 +1,5 @@
 library(plumber)
 setwd("/Users/klou/Documents/GitHub/r-klou/14-deployment/")
-a <- plumb("myfunction.R")
+a <- plumb("myopenapi.R")
 a$run(port=8000)
 
-pr("myfunction.R") %>%
-  pr_run(port=8000)
-
-pr("myopenai.R") %>%
-  pr_run(port=8000)
--- 14-deployment/myopenai.R
+++ 14-deployment/myopenai.R
@@ -1,11 +1,6 @@
 library(devtools)
-devtools::install_github("ben-aaron188/rgpt3")
+#devtools::install_github("ben-aaron188/rgpt3")
 library(rgpt3)
-setwd("/Users/klou/Documents/GitHub/")
-gpt3_authenticate("access_key.txt")
-c <- gpt3_single_completion(prompt_input = '說個冷笑話吧')
-c[[1]]$gpt3
-
 
 # get response
 #cat(response$choices[[1]]$text)
@@ -13,5 +8,25 @@ c[[1]]$gpt3
 #* @param msg The message to echo
 #* @get /echo
 function(msg="") {
-  list(msg = paste0("The message is: '", c[[1]]$gpt3, "'"))
+  setwd("/Users/klou/Documents/GitHub/")
+  gpt3_authenticate("access_key.txt")
+  c <- gpt3_single_completion(prompt_input = msg)
+  c[[1]]$gpt3
+  list(msg = paste0("我說：", c[[1]]$gpt3, "'"))
+}
+
+#* Plot a histogram
+#* @serializer png
+#* @get /plot
+function() {
+  rand <- rnorm(100)
+  hist(rand)
+}
+
+#* Return the sum of two numbers
+#* @param a The first number to add
+#* @param b The second number to add
+#* @post /sum
+function(a, b) {
+  as.numeric(a) + as.numeric(b)
 }
