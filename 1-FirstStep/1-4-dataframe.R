0041b3bd9d5121a59de9f29ba69f26e4c0556811
Kuo-Liang Ou <kloutw@gmail.com>
2025-10-12 19:58:47 +0800

    Update 1-4-dataframe.R

--- 1-FirstStep/1-4-dataframe.R
+++ 1-FirstStep/1-4-dataframe.R
@@ -1,4 +1,4 @@
-#make three vectors a,b,c
+#construct three vectors a,b,c
 a = c(2,3,4)
 b = c(5,6,7)
 c = c("A","B","C")
@@ -21,16 +21,14 @@ mydf[1,3]         #character and character   某一個值
 mydf[2]           #data.frame   回傳單一行之DF,請比較mydf[,2]
 mydf[,1:2]        #data.frame and list ,請小心與 python 的語法不同處
 
-typeof(mydf) # the type in memory
-class(mydf)  # the type in present
+typeof(mydf) # the type in memory : list
+class(mydf)  # the type in present : data.frame
 
 e = mydf$c        #set e to any one above for the next two commands
-mode(e)           #mostly as the same as type of, used in S language
-class(e)          #reveal the present of e
 typeof(e)         #reveal the data type of e
+class(e)          #reveal the present of e
 
-#mode() gives the "type" of object from the point of view of 
-#          S language
+
 #class() is used to define/identify what "type" an object is from 
 #        the point of view of object-oriented programming in R.
 #typeof() gives the "type" of object from R's point of view
