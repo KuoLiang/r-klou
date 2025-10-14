aae5e411be9f04bf85838a9ae347c4067dfc39ec
Kuo-Liang Ou <kloutw@gmail.com>
2025-10-14 15:50:10 +0800

    a

--- 2-preProcess/dplyr.R
+++ 2-preProcess/dplyr.R
@@ -1,7 +1,13 @@
-
+#tidyvese package includes:
+#ggplot2, 資料視覺化 
+#dplyr 數據處理 
+#tidyr 資料清理 
+#readr 資料讀取 
+#purrr 函式化程式設計 
+#tibble 資料型別定義 
 if (!require("dplyr")) install.packages("dplyr")
-
 library(dplyr)
+
 data(iris)
 typeof(iris)
 class(iris)
