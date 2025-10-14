32ade859d5ba4e9b2006e4cfdf4310297f742b95
Kuo-Liang Ou <kloutw@gmail.com>
2025-10-14 15:31:32 +0800

    a

---
+++ 2-preProcess/2-1-seq_rep.R
@@ -0,0 +1,21 @@
+#sequence   seq.int(from, to, by, length.out, along.with, ...)
+1:10        #a sequence
+seq(1:10)   #連續數
+seq(1,10)
+seq(from =1, to=10) #指定參數
+seq(1, 10, by=2)  #等差
+seq(1, 10, 2)     #等差
+seq(1.1, 10.1)   #from =1.1 ; to=10.1 ; by=1 預設值為1
+seq(1.1, 10.1, 1.1) #from =1.1 ; to=10.1 ; by=1.1
+seq(from=1,to=10,length.out=100)  #1至10之間硬要給出100個
+
+#repeat     rep(x, times)
+rep(1,10)
+rep(1.1,10)
+rep("hello",10)
+rep(c(1,3),10)
+rep(1:3,10)
+rep(NA,10)
+rep(c(1,3),each=5)
+rep(c(1,3),10)
+
