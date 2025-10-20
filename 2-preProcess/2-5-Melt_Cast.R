dd274ec4ed4f37423d0d8e5afb5650b473ca8800
Kuo-Liang Ou <kloutw@gmail.com>
2025-10-20 23:22:11 +0800

    Update 2-5-Melt_Cast.R

--- 2-preProcess/2-5-Melt_Cast.R
+++ 2-preProcess/2-5-Melt_Cast.R
@@ -17,10 +17,10 @@ df1 <- data.frame(
 
 df1
 ### melt to 2 cols with Col_Name and values
-#wide to long ,接長, 把欄位名稱變成新欄位的值
+#wide to long ,伸長, 把欄位名稱變成新欄位的值
 
 df_long <- melt(df1, 
-             measure.vars =c("SID", "Category","Value"),   #哪些欄位需要拉長
+             measure.vars =c("SID", "Category","Value"),   #哪些欄位需要依序往下伸長
              variable.name = c("Col_Name"))   #原來欄位名稱變成值後，該欄的新名稱
 df_long
 
@@ -29,7 +29,7 @@ df_long
 # Reshape data from long to wide format using dcast
 df_wide <- dcast(df1, SID ~ Category, value.var = "Value") #Y~X 指定 Y 軸 X 軸 
 df_wide
-df_wide <- dcast(df1, Category ~ Value, value.var = "SID") #Y~X 指定 Y 軸 X 軸 
-df_wide
+df_wide2 <- dcast(df1, Category ~ Value, value.var = "SID") #Y~X 指定 Y 軸 X 軸 
+df_wide2
 
 ################
