d976e68642585ade1cbfde77604e3c9893e71303
Kuo-Liang Ou <kloutw@gmail.com>
2025-10-21 08:36:28 +0800

    Update 2-5-Melt_Cast.R

--- 2-preProcess/2-5-Melt_Cast.R
+++ 2-preProcess/2-5-Melt_Cast.R
@@ -5,28 +5,22 @@ library(reshape2)
 ################
 df1 <- data.frame(
   SID = c(1, 2, 3, 4),
-  Dept = c("A", "B", "A", "B"),
+  Dept = c("CS", "Math", "CS", "Math"),
+  Grade = c(2, 2, 3, 4),
   Scores = c(10, 20, 30, 40)
 )
-# 若有空值時
-# df1 <- data.frame(
-#   SID = c(1, 2, 3, 4),
-#   Dept = c("A", "B", "A", "C"),
-#   Scores = c(10, 10, 20, 20)
-# )
-
 df1
-### melt to 2 cols with Col_Name and values
+### melt to cols with Col_Name and values
 #wide to long ,伸長, 把欄位名稱變成新欄位的值
 #melt(data,id.vars,measure.vars,variable.name = "variable")
 
 df_long <- melt(df1, 
-             measure =c("SID", "Dept","Scores"),   #哪些欄位需要依序往下伸長
+             measure =c("SID", "Dept","Grade","Scores"),   #哪些"欄位"需要依序往下伸長
              variable = c("Col_Name"))   #原來欄位名稱變成值後，該欄的新名稱
 df_long
 df_long2 <- melt(df1, 
                 id=c("SID"),
-                measure =c("Dept","Scores"),   #哪些"欄位"需要依序往下伸長
+                measure =c("Dept","Scores"),   #只挑部分"欄位"需要依序往下伸長
                 variable = c("Col_Name"))   #原來欄位名稱變成值後，該欄的新名稱
 df_long2
 
@@ -37,7 +31,7 @@ df_long2
 
 df_wide <- dcast(df1, SID ~ Dept, value.var = "Scores") #Y~X 指定 Y 軸 X 軸 
 df_wide
-df_wide2 <- dcast(df1, Dept ~ SID, value.var = "Scores") #Y~X 指定 Y 軸 X 軸 
+df_wide2 <- dcast(df1, SID ~ Dept+Grade, value.var = "Scores") #Y~X 指定 Y 軸 X 軸 
 df_wide2
 
 ################
