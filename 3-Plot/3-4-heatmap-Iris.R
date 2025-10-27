a6cd28f7eeb5180c9ea877b20b0c64d49203f7ec
Kuo-Liang Ou <kloutw@gmail.com>
2025-10-27 15:48:37 +0800

    Update 3-4-heatmap-Iris.R

--- 3-Plot/3-4-heatmap-Iris.R
+++ 3-Plot/3-4-heatmap-Iris.R
@@ -6,12 +6,32 @@
 if (!require("ggplot2")) install.packages("ggplot2")
 library(ggplot2)
 
-irisMatrix = as.matrix(dist(iris[,1:4]))
+#################################################
+### matrix 介紹
+# matrix 物件是由包含相同的元素組成的2維物件
+#################################################
+#matrix(data=NA, nrow=1, ncol=1, byrow=FALSE, dimnames=NULL)
+mdat <- matrix(c(1,2,3, 11,12,13), nrow = 2, ncol = 3, byrow = TRUE,
+               dimnames = list(c("row1", "row2"),
+                               c("C.1", "C.2", "C.3")))
+mdat
+
+a = c(1,2,3)
+b= as.matrix(dist(a))
+
+c=matrix(1:12, 3, 4)    #建立 3列 * 4 行矩陣 bycol
+c
+d=matrix(1:12, ncol=4)
+d
+e= matrix(1:12) 
+e
+
+###
+
+irisMatrix = as.matrix(dist(iris[,1:4])). #dist() Distance Matrix Computation，euclidean distance is the default
 #irisMatrix = as.matrix(iris)  #error , x must be a numeric matrix
 irisMatrix
 heatmap(irisMatrix)
-heatmap(irisMatrix, scale="column")
-heatmap(irisMatrix, scale="row")
 
 #################################################
 #Pretty Heatmap
@@ -47,22 +67,5 @@ library("scatterplot3d")
 scatterplot3d(iris)
 scatterplot3d(iris$Sepal.Length,iris$Sepal.Width,iris$Petal.Length)
 
-#################################################
-### matrix 介紹
-#################################################
-#matrix(data=NA, nrow=1, ncol=1, byrow=FALSE, dimnames=NULL)
-mdat <- matrix(c(1,2,3, 11,12,13), nrow = 2, ncol = 3, byrow = TRUE,
-               dimnames = list(c("row1", "row2"),
-                               c("C.1", "C.2", "C.3")))
-mdat
 
-a = c(1,2,3)
-b= as.matrix(dist(a))
-
-c=matrix(1:12, 3, 4)    #建立 3列 * 4 行矩陣 bycol
-c
-d=matrix(1:12, ncol=4)
-d
-e= matrix(1:12) 
-e
 
