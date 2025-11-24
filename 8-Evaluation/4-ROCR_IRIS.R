1661800830453c83a749f6afc2959a7799b38aeb
Kuo-Liang Ou <kloutw@gmail.com>
2025-11-24 23:19:29 +0800

    Update 4-ROCR_IRIS.R

--- 8-Evaluation/4-ROCR_IRIS.R
+++ 8-Evaluation/4-ROCR_IRIS.R
@@ -2,27 +2,37 @@
 #Editor: Kuo-Liang Ou
 #National Tsing Hua University ILST
 ##########################################
-install.packages("ROCR")
+
+if (!require("e1071")) install.packages("e1071")
+library(e1071)
+if (!require("ROCR")) install.packages("ROCR")
 library(ROCR)
 data(ROCR.simple)
-library(e1071)
-
-group=c(rep(0,times=100),rep(1,times=100))
-#pred <- prediction( ROCR.simple$labels, ROCR.simple$labels) 
-pred <- prediction( rnorm(200,mean=1,sd=1), ROCR.simple$labels) 
-pred <- prediction( rnorm(200,mean=2,sd=2), ROCR.simple$labels) 
-
-pred <- prediction( rnorm(200,mean=1,sd=1), group) 
-pred <- prediction( rnorm(200,mean=2,sd=2), group) 
+set.seed(2025)
+group=c(rep(0,times=100),rep(1,times=100)) #分兩組
+#pred <- prediction( ROCR.simple$labels, ROCR.simple$labels)
 
 # prediction 第一個參數位資料集，第二個參數為組別，目前支援二分法
 # 多組以上需兩兩比較
+pred1 <- prediction( rnorm(200,mean=1,sd=1), ROCR.simple$labels) 
+pred1 <- prediction( rnorm(200,mean=2,sd=2), ROCR.simple$labels) 
 
 perf <- performance(pred,"tpr","fpr") 
 perf
 plot(perf)
 perf <- performance(pred, measure = "auc")   #area under curve
 perf@y.values
+
+
+########################################
+pred2 <- prediction( rnorm(200,mean=1,sd=1), group) 
+pred2 <- prediction( rnorm(200,mean=2,sd=2), group) 
+
+perf2 <- performance(pred2,"tpr","fpr") 
+perf2
+plot(perf2)
+perf2 <- performance(pred2, measure = "auc")   #area under curve
+perf2@y.values
 
 #########
 #for IRIS
